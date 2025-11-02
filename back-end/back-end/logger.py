4574943e948d2b614675e498423a4e1c7d22ccb1
GabrysiaCzer <gabrysia.czernecka@gmail.com>
2025-11-02 15:49:13 +0100

    fix: cleanup folder structure and remove duplicate backend

--- back-end/back-end/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    filename="app_errors.log",
-)
-LOGGER = logging.getLogger(__name__)
